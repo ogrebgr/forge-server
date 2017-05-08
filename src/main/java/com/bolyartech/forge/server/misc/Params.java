0ebb3c0c43dc95139e0b39c7cf6af47012e0369d
Ognyan Bankov <ogibankov@gmail.com>
2017-05-08 16:50:10 +0300

    adding utility methods

--- src/main/java/com/bolyartech/forge/server/misc/Params.java
+++ src/main/java/com/bolyartech/forge/server/misc/Params.java
@@ -1,12 +1,21 @@
 package com.bolyartech.forge.server.misc;
 
+import com.bolyartech.forge.server.route.InvalidParameterValue;
+import com.bolyartech.forge.server.route.MissingParameterValue;
+import com.bolyartech.forge.server.route.RequestContext;
 import com.google.common.base.Strings;
+
+import static com.bolyartech.forge.server.misc.ForgeMessageFormat.format;
 
 
 /**
  * Utility class for GET/POST/PI parameters
  */
 public class Params {
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+
+
     /**
      * Non-instantiable utility class
      */
@@ -36,6 +45,114 @@ public class Params {
         }
 
         return ret;
+    }
+
+
+    /**
+     * Extracts long parameter's value from POST parameters
+     *
+     * @param ctx           Context from which the value will be extracted
+     * @param parameterName Parameter name
+     * @return extracted value
+     * @throws MissingParameterValue if there is no value for this parameter
+     * @throws InvalidParameterValue if the value is present but cannot be parsed as long
+     */
+    public static long extractLongFromPost(@NonNull RequestContext ctx, @NonNull String parameterName)
+            throws MissingParameterValue, InvalidParameterValue {
+
+        String s = ctx.getFromPost(parameterName);
+
+        return extractLongHelper(parameterName, s, POST);
+    }
+
+
+    /**
+     * Extracts long parameter's value from GET parameters
+     *
+     * @param ctx           Context from which the value will be extracted
+     * @param parameterName Parameter name
+     * @return extracted value
+     * @throws MissingParameterValue if there is no value for this parameter
+     * @throws InvalidParameterValue if the value is present but cannot be parsed as long
+     */
+    public static long extractLongFromGet(@NonNull RequestContext ctx, @NonNull String parameterName)
+            throws MissingParameterValue, InvalidParameterValue {
+
+        String s = ctx.getFromGet(parameterName);
+
+        return extractLongHelper(parameterName, s, GET);
+    }
+
+
+    private static long extractLongHelper(@NonNull String parameterName,
+                                          @Nullable String value,
+                                          @NonNull String type)
+            throws MissingParameterValue, InvalidParameterValue {
+
+        if (value == null) {
+            throw new MissingParameterValue("Missing " + type + " value for parameter " + parameterName);
+        }
+
+        try {
+            return Long.parseLong(value);
+        } catch (NumberFormatException e) {
+            throw new InvalidParameterValue(format("Invalid value {} for parameter {}",
+                    value, parameterName));
+        }
+    }
+
+
+    /**
+     * Extracts integer parameter's value from POST parameters
+     *
+     * @param ctx           Context from which the value will be extracted
+     * @param parameterName Parameter name
+     * @return extracted value
+     * @throws MissingParameterValue if there is no value for this parameter
+     * @throws InvalidParameterValue if the value is present but cannot be parsed as int
+     */
+    public static long extractIntFromPost(@NonNull RequestContext ctx, @NonNull String parameterName)
+            throws MissingParameterValue, InvalidParameterValue {
+
+        String s = ctx.getFromPost(parameterName);
+
+        return extractIntHelper(parameterName, s, POST);
+    }
+
+
+    /**
+     * Extracts integer parameter's value from GET parameters
+     *
+     * @param ctx           Context from which the value will be extracted
+     * @param parameterName Parameter name
+     * @return extracted value
+     * @throws MissingParameterValue if there is no value for this parameter
+     * @throws InvalidParameterValue if the value is present but cannot be parsed as int
+     */
+    public static long extractIntFromGet(@NonNull RequestContext ctx, @NonNull String parameterName)
+            throws MissingParameterValue, InvalidParameterValue {
+
+        String s = ctx.getFromGet(parameterName);
+
+        return extractIntHelper(parameterName, s, GET);
+    }
+
+
+    private static long extractIntHelper(@NonNull String parameterName,
+                                         @Nullable String value,
+                                         @NonNull String type)
+            throws MissingParameterValue, InvalidParameterValue {
+
+        if (value == null) {
+            throw new MissingParameterValue("Missing " + type + " value for parameter " + parameterName);
+        }
+
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new InvalidParameterValue(format("Invalid value {} for parameter {}",
+                    value, parameterName));
+        }
     }
 
 }
