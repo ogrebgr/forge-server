a16ad37a138ac89d1ea8ea2d98dfe611a4fb9486
Ognyan Bankov <ogibankov@gmail.com>
2022-08-06 14:46:22 +0300

    new versions

---
+++ src/main/java/com/bolyartech/forge/server/misc/TimeProvider.kt
@@ -0,0 +1,26 @@
+package org.example.kforge.misc
+
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import javax.inject.Inject
+
+interface TimeProvider {
+    fun getVmTime(): Long
+    fun getWallClockTime(): LocalDateTime
+    fun getWallClockTimeMillis(): Long
+}
+
+
+class TimeProviderImpl @Inject constructor() : TimeProvider {
+    override fun getVmTime(): Long {
+        return System.nanoTime() / 1000000
+    }
+
+    override fun getWallClockTime(): LocalDateTime {
+        return LocalDateTime.now(ZoneOffset.UTC)
+    }
+
+    override fun getWallClockTimeMillis(): Long {
+        return System.currentTimeMillis()
+    }
+}
