c4bfc13ab9cbbf40d3405b9bed6b5c65ab107c1f
Ognyan Bankov <bankov@bolyar.com>
2025-10-17 16:26:09 +0300

    cosmetics

--- src/main/kotlin/com/bolyartech/forge/server/db/DbPool.kt
+++ src/main/kotlin/com/bolyartech/forge/server/db/DbPool.kt
@@ -11,7 +11,7 @@ interface DbPool {
      * Returns database connection
      *
      * @return database connection
-     * @throws SQLException if error occur in the db driver
+     * @throws SQLException if error occurs in the db driver
      */
     @get:Throws(SQLException::class)
     val connection: Connection
