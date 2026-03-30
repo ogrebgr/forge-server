4021aa9c1d7204ed2ea6aaee252475a648d9658a
Ognyan Bankov <bankov@bolyar.com>
2026-03-30 17:57:22 +0300

    bugfix: was not able to get the body

--- src/main/kotlin/com/bolyartech/forge/server/response/builders/OkHtmlResponseBuilder.kt
+++ src/main/kotlin/com/bolyartech/forge/server/response/builders/OkHtmlResponseBuilder.kt
@@ -1,12 +1,9 @@
 package com.bolyartech.forge.server.response.builders
 
-import com.bolyartech.forge.server.response.HtmlResponse
 import jakarta.servlet.http.HttpServletResponse
 
-class OkHtmlResponseBuilder(val body: String) : HtmlResponseBuilder(HttpServletResponse.SC_OK) {
-    override fun build(): HtmlResponse {
-        body(body)
-
-        return super.build()
+class OkHtmlResponseBuilder(private val body: String) : HtmlResponseBuilder(HttpServletResponse.SC_OK) {
+    init {
+        super.body(body)
     }
 }
