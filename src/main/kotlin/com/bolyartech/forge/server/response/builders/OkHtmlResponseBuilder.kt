998ee6de3d9f66071ce2e81532083a7b7cdd4d40
Ognyan Bankov <bankov@bolyar.com>
2026-03-30 17:49:22 +0300

    bugfix: getBody() was returning "" before calling build, causing confusion

--- src/main/kotlin/com/bolyartech/forge/server/response/builders/OkHtmlResponseBuilder.kt
+++ src/main/kotlin/com/bolyartech/forge/server/response/builders/OkHtmlResponseBuilder.kt
@@ -3,7 +3,7 @@ package com.bolyartech.forge.server.response.builders
 import com.bolyartech.forge.server.response.HtmlResponse
 import jakarta.servlet.http.HttpServletResponse
 
-class OkHtmlResponseBuilder(private val body: String) : HtmlResponseBuilder(HttpServletResponse.SC_OK) {
+class OkHtmlResponseBuilder(val body: String) : HtmlResponseBuilder(HttpServletResponse.SC_OK) {
     override fun build(): HtmlResponse {
         body(body)
 
